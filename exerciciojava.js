9281b334e6f462f1d517a688cca9521f4272bf34
Sylvia Lira <123954079+sylvialira@users.noreply.github.com>
2024-03-20 14:52:28 -0400

    javascript exercicios

---
+++ exerciciojava.js
@@ -0,0 +1,52 @@
+//Repeticao
+
+// const familia = ['Bruno', 'Nina', 'Thiago', 'Noah']
+
+// familia.forEach((element, index) => {
+// console.log('Execução: ', index + 1)
+// console.log('Nome: ' + element)
+// console.log('-------------------')
+
+// })
+
+//Decisao
+
+// const cidades = ['Sao Paulo', 'Rio de Janeiro', 'Florianopolis', 'Recipe']
+
+//     cidades.forEach((element, index) => {
+//        // if ( cidades == 'Florianopolis'){
+
+//     console.log('Execução: ', index + 1)
+//     console.log('Cidade: ' + element)
+//     if ( element == 'Florianopolis'){
+//     console.log('Encontrado')
+// } else {    
+//     console.log('Nao Encontrado')
+// }
+//     console.log('-------------------')
+
+// })
+
+//Funcao modo 1
+
+// function somaDeValores(numero1, numero2){
+
+//    const soma = numero1+numero2
+
+//     return 'Numero1:' + numero1 + 'Numero2:' + numero2 + 'Soma' + soma
+// }
+
+// console.log(somaDeValores(20, 10))
+
+//Funcao modo 2
+
+function somaDeValores(numero1, numero2){
+
+   const soma = numero1+numero2
+
+    console.log('Numero1: ' + numero1)
+    console.log('Numero2: ' + numero2)
+    console.log('Soma: ' + soma)
+}
+
+somaDeValores(20, 10)
