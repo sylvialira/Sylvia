4885202fd7ced0dd7c4ddaf93c71101e7d8dfaa3
Sylvia Lira <123954079+sylvialira@users.noreply.github.com>
2024-04-24 12:06:06 -0400

    test: add new playwright test

--- playwright/tests/sylius/login.spec.ts
+++ playwright/tests/sylius/login.spec.ts
@@ -2,31 +2,26 @@ import { test, expect } from "@playwright/test";
 
 test("User should be able to", async ({ page }) => {});
 
-test.only("login", async ({ page }) => {
+test("login", async ({ page }) => {
   await page.goto("https://demo.sylius.com/en_US/");
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Sylius/);
-
-  // await page.locator(loginElements.button.loginPageLogin).click();
 
   await page.getByRole("link", { name: "Login" }).click();
-
-  // await page.locator(loginElements.field.email).fill(email);
-  // await page.waitForTimeout(500);
-  // await page.keyboard.press("Tab");
-  // await page.locator(loginElements.field.password).fill(password);
-  // await page.locator(loginElements.button.loginPageLogin).click();
-
-  //await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  // await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  // await expect(
-  //   page.getByRole("heading", { name: "Installation" })
-  // ).toBeVisible();
-
-  //   Username: shop@example.com
-  // Password: sylius
+  await page.getByLabel("Username").click();
+  await page.getByLabel("Username").fill("shop@example.com");
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill("sylius");
+  await page.getByRole("button", { name: "Login" }).click();
 });
+
+//await expect(page).toHaveTitle(/Sylius/);
+
+//await page.locator(loginElements.field.email).fill(email);
+// await page.waitForTimeout(500);
+// await page.keyboard.press("Tab");
+// await page.locator(loginElements.field.password).fill(password);
+// await page.locator(loginElements.button.loginPageLogin).click();
+
+// Expects page to have a heading with the name of Installation.
+// await expect(
+//   page.getByRole("heading", { name: "Installation" })
+// ).toBeVisible();
